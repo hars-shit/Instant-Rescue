e1a950934bb2c2f6a64ed78656dbbdd9513a5808
harshit <upadhyayharshit05@gmail.com>
2023-07-02 16:13:49 +0530

    fifth commit

--- src/MainPages/Jsx/Map.jsx
+++ src/MainPages/Jsx/Map.jsx
@@ -17,7 +17,7 @@ const Map = ({long,lat}) => {
       container: mapContainerRef.current,
       center:[long,lat],
       // center: [79.331769,	30.404105], // Initial center coordinates
-      zoom: 10, // Initial zoom level
+      zoom: 8, // Initial zoom level
     });
 
   setMap(showmap)
