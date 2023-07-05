a0aaf7a2a970d4c14c4916a783649a6dbf3196bf
harshit <upadhyayharshit05@gmail.com>
2023-07-05 17:54:01 +0530

    sixteenth commit

--- src/MainPages/Jsx/Navbar.jsx
+++ src/MainPages/Jsx/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import '../Styles/Navbar.css'
 import {BiMenuAltRight} from 'react-icons/bi'
+import {FaStreetView,FaLocationArrow} from 'react-icons/fa'
 
 
 const Navbar=()=>{
@@ -11,18 +12,24 @@ const Navbar=()=>{
       <button className="navbar-toggler btn-icon" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
       <BiMenuAltRight id="sidemenu-icon"/>
       </button>
-      <div className="offcanvas offcanvas-end text-bg-dark"  id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+      <div className="offcanvas offcanvas-end "  id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
         <div className="offcanvas-header">
-          <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>
+          <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel"><FaStreetView />Location</h5>
           <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
-        <div className="offcanvas-body">
+        <div className="offcanvas-body ">
           <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to={'/SignUp'}>SignUp</Link>
+              <Link className="nav-link active" aria-current="page" to={'/SignUp'}><FaLocationArrow />SignUp</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" to="/">Link</a>
+              <Link className="nav-link active" to="https://github.com/hars-shit/Instant-Rescue"><FaLocationArrow />About</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" to="/APIs"><FaLocationArrow />APIs</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" to="/FAQs">FAQs</Link>
             </li>
            
           </ul>
