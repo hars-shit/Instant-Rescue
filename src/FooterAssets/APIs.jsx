e2c8541b4be9bcc34e0e4c066b002a30936370dd
harshit <upadhyayharshit05@gmail.com>
2023-07-06 14:09:43 +0530

    tweentyfourth commit

--- src/FooterAssets/APIs.jsx
+++ src/FooterAssets/APIs.jsx
@@ -1,6 +1,22 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import './APIs.css'
+import {FiArrowUpRight} from 'react-icons/fi';
+import Navbar from "../MainPages/Jsx/Navbar";
 const APIs=()=>{
-    return <h2>APIs</h2>
+    return  (
+
+        <>
+        <Navbar />
+    <div className="main-api">
+        <div className="api-heading">APIs Utilized</div>
+        <div className="cards">
+        <div className="api-card"><Link>tom tom map api<FiArrowUpRight /></Link></div>
+        <div className="api-card geo-card"><Link >geoapify reverse geocoding <FiArrowUpRight /></Link></div>
+        <div className="api-card"><Link to={"https://www.npmjs.com/package/react-geolocated"}>react-geolocated<FiArrowUpRight /></Link></div>
+        </div>
+    </div>
+    </>
+        )
 }
 export default APIs;
