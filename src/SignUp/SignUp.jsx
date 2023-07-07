42f4c1f9156acc2d01ce32a8392b2bc78b6d671f
harshit <upadhyayharshit05@gmail.com>
2023-07-07 18:58:44 +0530

    latest commit

--- src/SignUp/SignUp.jsx
+++ src/SignUp/SignUp.jsx
@@ -1,27 +1,42 @@
 import React, {useState } from "react";
 import '../SignUp/SignUp.css'
 import { useDispatch } from "react-redux";
-import FormData, { saveFormData } from "../Store/slices/FormData";
+import { saveFormData } from "../Store/slices/FormData";
+import {GoAlertFill} from 'react-icons/go'
 const SignUp=()=>{
  
- 
+  // const [correctPhNo,setCorrectPhNo]=useState(true);
+  // const [correctOcc,setCorrectOcc]=useState(true);
+  // const [image,setImage]=useState();
  const [data,setData]=useState({
     name:"",
     pNo:"",
-
+    occupation:"",
+    image:"",
   })
   const dispatch=useDispatch();
   const handleChange=(e)=>{
    const name=e.target.name;
    const value=e.target.value;
    setData({...data,[name]:value})
-  }
+
+}
+
+  
+  
+  
   const handleSubmit=(e)=>{
    e.preventDefault();
-   dispatch(saveFormData(data));
-   console.log(data)
-  //  saveFormData(data)
-  console.log('hello')
+    if(data.occupation==0 ){
+      setCorrectOcc(!correctOcc); 
+    }
+
+    else{
+      dispatch(saveFormData(data));
+      console.log(data)
+    }
+    
+ 
   }
 
     return(
@@ -37,24 +52,28 @@ const SignUp=()=>{
     </div>
 
     <div className="form-floating">
-      <input type="text" className="form-control" id="floatingInput" value={data.name} name="name" onChange={handleChange}  placeholder="Dr. Kamlesh Bhatt" />
+      <input type="text" className="form-control" id="floatingInput" value={data.name} name="name" onChange={handleChange}   placeholder="name"/>
       <label >Name</label>
     </div>
     <div className="form-floating">
-      <input type="number" className="form-control" value={data.pNo} name="pNo" id="floatingInput" onChange={handleChange}  placeholder="phone number"  />
+      <input type="number" className="form-control" value={data.pNo} name="pNo" id="floatingInput" onChange={handleChange} placeholder={"phone number"} />
+     
       <label >phone number</label>
+
     </div>
     <div className="form-floating">
-      <select className="form-control">
-        <option placeholder="public hospital" id="option-choice" >---select-one-option---</option>
-        <option>private hospital</option>
-        <option  >public hospital </option>
-        <option >pharmacy store</option>
+      <select className="form-control" name="occupation" value={data.occupation} onChange={handleChange}>
+     
+          <option placeholder="public hospital"    id="option-choice" value={0} >---select-one-option---</option>
+          
+        <option  name="privateHos" value="private hospital"> private hospital</option>
+        <option  name="publicHos" value="public hospital">public hospital </option>
+        <option  name="pharmacyStore" value="pharmacy store">pharmacy store</option>
       </select>
       <label >occupation</label>
     </div>
     <div className="form-floating">
-      <input type="file" className="form-control"  id="floatingInput" placeholder="image"  />
+      <input type="file" accept="Image"  name="image" value={data.image}  className="form-control"  id="floatingInput" placeholder="image"  onChange={handleChange}/>
       <label >image </label>
     </div>
    
@@ -73,4 +92,4 @@ const SignUp=()=>{
 </div>
     )
 }
-export default SignUp
+export default SignUp;
